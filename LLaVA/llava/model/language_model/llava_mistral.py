4aecc657b0f275b4fa0002c26ba86bf75a886c14
Youjia Zhang <zhangyoujia@skku.edu>
2026-07-20 17:57:36 +0900

    Update llava_mistral.py

--- LLaVA/llava/model/language_model/llava_mistral.py
+++ LLaVA/llava/model/language_model/llava_mistral.py
@@ -30,10 +30,8 @@ from transformers.generation.utils import GenerateOutput
 if 'BASELINE' in os.environ and os.environ['BASELINE'] == 'OURS':
     if 'ARCHIVE_MODE' in os.environ and os.environ['ARCHIVE_MODE'] == 'liuhaotian/llava-v1.6-vicuna-7b':
         from ..llava_arch_llava_next_7b import LlavaMetaModel, LlavaMetaForCausalLM
-    else:
-        from ..llava_arch_default  import LlavaMetaModel, LlavaMetaForCausalLM
 else:
-    from ..llava_arch_default import LlavaMetaModel, LlavaMetaForCausalLM   
+    from ..llava_arch import LlavaMetaModel, LlavaMetaForCausalLM   
 
 
 class LlavaMistralConfig(MistralConfig):
